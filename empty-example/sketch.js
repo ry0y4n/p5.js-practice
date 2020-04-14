503ca70cc0bbe45d28b9e0e41e555d19a85ea923
ry0y4n <rusk.ryoy4n@gmail.com>
2020-04-14 18:32:13 +0900

    Coordinates

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,7 +1,20 @@
 function setup() {
   // put setup code here
+  createCanvas(720, 400);
 }
 
 function draw() {
   // put drawing code here
+  background(0);
+  noFill();
+
+  stroke(255);
+  point(width*0.5, height*0.5);
+  point(width*0.25, height*0.25);
+
+  stroke(0, 153, 255);
+  line(0, height*0.33, width, height*0.33);
+
+  stroke(255, 153, 0);
+  rect(width*0.25, height*0.1, width*0.5, height*0.8);
 }
